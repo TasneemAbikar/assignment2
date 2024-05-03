be5f027ebfd6834ea4a0e60a3ec5f3d63ccefad6
chaturya doddi <131220115+Chatu4@users.noreply.github.com>
2024-05-03 22:19:31 +1000

    GuestUser.java

--- GuestUser.java
+++ GuestUser.java
@@ -1,24 +1,22 @@
 import java.util.List;
 import java.util.Random;
-import java.awt.Image;
 
 public class GuestUser {
     protected String sessionID;
     protected int sessionTime;
 
-    public GuestUser(String sessionID, int sessionTime) {
-        this.sessionID = sessionID;
-        this.sessionTime = sessionTime;
+    private Account createAccount(String firstName, String lastName, String email, String password) {
+        // Generate a random account ID
+        Random rand = new Random();
+        int accountID = rand.nextInt(1000000); // Adjust the range as needed
+        
+        // Create and return an account with the generated ID
+        return new Account(accountID, firstName, lastName, email, password);
     }
 
-    private void createAccount(String firstName, String lastName, String email, String password) {
-        Account account = new Account(firstName, lastName, email, password); // need to randomly generate the accountID in the account class.
-        // the user is then redirected to login for the first time
-    }   
-    public List searchForQuestions(String query, Boolean filters) { 
-        SearchClass newQuery = new SearchClass(query, filters);
-        List results = newQuery.returnResults(); //searchclass needs to have method that is called to re/turn a list result. 
-        return results;
+    public List<Post> searchForQuestions(String query, boolean filters) {
+        // Create a new search instance and return results
+        Search search = new Search(query, filters);
+        return search.returnResults();
     }
 }
-
