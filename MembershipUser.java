d97080238547b5396dde3aae23f7effc0b351ef3
chaturya doddi <131220115+Chatu4@users.noreply.github.com>
2024-05-04 13:06:22 +1000

    Update MembershipUser.java

--- MembershipUser.java
+++ MembershipUser.java
@@ -1,13 +1,12 @@
 import java.util.List;
-import java.util.Random;
 import java.awt.Image;
 
-public class MembershipUser extends RegisteredUser{
+public class MembershipUser extends RegisteredUser {
     private String membershipType;
     private int remainingMembershipPlan;
 
-    public MembershipUser(String membershipType, int remainingMembershipPlan, String profile, Image profilepic, List existingQuestions, List existingAnswers, List existingComments, Boolean profileType, String sessionID, int sessionTime) {
-        super(profile,profilepic,existingQuestions,existingAnswers,existingComments,profileType,sessionID,sessionTime);
+    public MembershipUser(String membershipType, int remainingMembershipPlan, String profile, Image profilepic, List<Post> existingQuestions, List<Post> existingAnswers, List<String> existingComments, Boolean profileType, String sessionID, int sessionTime) {
+        super(profile, profilepic, existingQuestions, existingAnswers, existingComments, profileType, sessionID, sessionTime);
         this.membershipType = membershipType;
         this.remainingMembershipPlan = remainingMembershipPlan;
     }
@@ -15,11 +14,14 @@ public class MembershipUser extends RegisteredUser{
     public void boostSearchQueries(Post post) {
         post.addBoost();
     }
+
     public void cancelMembership() {
         membershipType = "None";
         remainingMembershipPlan = 0;
     }
+
     public void requestExpertResponse(Post post) {
-        
+        ExpertRequestUser expertRequestUser = new ExpertRequestUser();
+        expertRequestUser.addRequest(post);
     }
 }
