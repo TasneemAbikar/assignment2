03d13acc1d584ab0d937bec6a9b08c077545ca13
chaturya doddi <131220115+Chatu4@users.noreply.github.com>
2024-05-04 13:07:45 +1000

    GuestUser.java

--- GuestUser.java
+++ GuestUser.java
@@ -5,11 +5,15 @@ public class GuestUser {
     protected String sessionID;
     protected int sessionTime;
 
-    private Account createAccount(String firstName, String lastName, String email, String password) {
+    public GuestUser(String sessionID, int sessionTime) {
+        this.sessionID = sessionID;
+        this.sessionTime = sessionTime;
+    }
+
+    public Account createAccount(String firstName, String lastName, String email, String password) {
         Random rand = new Random();
         int accountID = rand.nextInt(10000);
-        //for the 10000, its the max amount of accounts that can be created in the platform (infinity seemed to be working a bit wrong, so i just chose 10000)
-        
+
         return new Account(accountID, firstName, lastName, email, password);
     }
 
